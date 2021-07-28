59dd3327ee458d3a952a8e7848479191267ca809
kyh196201 <kyh196201@gmail.com>
2021-07-28 18:03:37 +0900

    eslint, prettier 설정 추가

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+
+  plugins: ['html', 'import', 'prettier'],
+  extends: ['eslint:recommended', 'airbnb-base', 'prettier'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        trailingComma: 'all',
+        bracketSpacing: false,
+        printWidth: 80,
+        tabWidth: 2,
+        semi: true,
+        arrowParens: 'avoid',
+        endOfLine: 'auto',
+      },
+      {
+        usePrettierrc: false,
+      },
+    ],
+    'no-unused-vars': 'error',
+  },
+};
---
+++ Programmers/Level4/60060/solution.js
@@ -0,0 +1,10 @@
+(function () {
+  class Node {
+    constructor(count = 0, value = '') {
+      this.count = count;
+      this.value = value;
+      this.child = {};
+      this.isEnd = false;
+    }
+  }
+})();
