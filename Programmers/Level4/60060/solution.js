e1001f22bb5de9a3d392b2cfc803076d4f243d22
kyh196201 <kyh196201@gmail.com>
2021-07-29 13:42:10 +0900

    60060 풀이 작성 완료

--- Programmers/Level4/60060/solution.js
+++ Programmers/Level4/60060/solution.js
@@ -14,19 +14,21 @@
     }
 
     insert(word) {
+      this.root.count += 1;
       let currentNode = this.root;
       const wordLength = word.length;
 
       for (let i = 0; i < wordLength; i += 1) {
         const char = word[i];
-        currentNode.count += 1;
 
         if (currentNode.childNodes[char]) {
           currentNode = currentNode.childNodes[char];
         } else {
-          currentNode.childNodes[char] = new Node(char, 1);
+          currentNode.childNodes[char] = new Node(char, 0);
           currentNode = currentNode.childNodes[char];
         }
+
+        currentNode.count += 1;
       }
     }
 
@@ -56,4 +58,49 @@
   const queries = ['fro??', '????o', 'fr???', 'fro???', 'pro?'];
 
   //  예상 정답: [3, 2, 4, 1, 0]
+  function solution(words, queries) {
+    const queriesLength = queries.length;
+    const answer = [];
+
+    const trieArray = new Array(10001);
+
+    // Trie 세팅
+    words.forEach(word => {
+      const indexByLength = word.length;
+
+      if (!Array.isArray(trieArray[indexByLength])) {
+        trieArray[indexByLength] = [];
+        trieArray[indexByLength].push(new Trie(), new Trie());
+      }
+
+      const reversedWord = word.split('').reverse().join('');
+      trieArray[indexByLength][0].insert(word);
+      trieArray[indexByLength][1].insert(reversedWord);
+    });
+
+    // 정답 추출
+    for (let i = 0; i < queriesLength; i += 1) {
+      const query = queries[i];
+      const queryLength = query.length;
+      const useReverse = query.charAt(0) === '?';
+      let count = 0;
+      const newQuery = query.replace(/\?/g, '');
+
+      const trie = trieArray[queryLength];
+
+      if (trie) {
+        if (useReverse) {
+          count = trie[1].search(newQuery.split('').reverse().join(''));
+        } else {
+          count = trie[0].search(newQuery);
+        }
+      }
+
+      answer[i] = count;
+    }
+
+    return answer;
+  }
+
+  console.log(solution(words, queries));
 })();
