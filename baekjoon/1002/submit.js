5237d81ab409ce5fc33cb3b2b8654d9cbbfcceb7
kyh196201 <53055051+kyh196201@users.noreply.github.com>
2021-09-18 19:26:52 +0900

    백준 1002번 풀이

---
+++ baekjoon/1002/submit.js
@@ -0,0 +1,46 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function solution(x1, y1, r1, x2, y2, r2) {
+  const getDistance = (p1, p2) =>
+    Math.sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2);
+
+  const point1 = [x1, y1];
+  const point2 = [x2, y2];
+
+  //   두 원의 중심 좌표 사이의 거리
+  const d = getDistance(point1, point2);
+  let result = 0;
+
+  if (d === 0 && r1 === r2) {
+    result = -1;
+  } else if (d > r1 + r2 || d < Math.abs(r1 - r2)) {
+    //   두 원이 서로 외접하거나 내접하지 않는 경우
+    result = 0;
+  } else if (d === r1 + r2 || d === Math.abs(r1 - r2)) {
+    //   원이 외접하거나 내접할 경우
+    result = 1;
+  } else if (d < r1 + r2) {
+    //   접점이 2개일 경우
+    result = 2;
+  }
+
+  console.log(result);
+}
+
+const input = [];
+
+rl.on('line', line => {
+  input.push(line);
+}).on('close', () => {
+  for (let i = 1; i < input.length; i += 1) {
+    const numbers = input[i].split(' ').map(Number);
+    solution(...numbers);
+  }
+
+  process.exit();
+});
