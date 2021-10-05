99f69e18f4c10a42456e1975bc879fa479dcfb6d
kyh196201 <kyh196201@gmail.com>
2021-10-05 15:11:00 +0900

    백준 1018번 최초 풀이

--- baekjoon/1018/submit.js
+++ baekjoon/1018/submit.js
@@ -6,6 +6,8 @@ const rl = readline.createInterface({
 });
 
 const input = [];
+const whiteFirstRow = ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'];
+const blackFirstRow = ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'];
 
 rl.on('line', line => {
   input.push(line);
@@ -17,25 +19,43 @@ rl.on('line', line => {
 
   const plates = input.map(v => v.split(''));
 
-  let minCount = 0;
+  // 8 * 8 개수의 칸을 모두 바꾸는 경우가 64이므로 최초 시작값을 64로 설정
+  let minCount = 64;
 
   for (let row = 0; row <= N - 8; row += 1) {
     for (let col = 0; col <= M - 8; col += 1) {
-      let count = 0;
+      // 검은색 칸부터 시작하는 경우에 바꿔야하는 칸의 개수
+      let blackFirstCount = 0;
+      // 흰색 칸부터 시작하는 경우에 바꿔야하는 칸의 개수
+      let whiteFirstCount = 0;
 
-      for (let i = row; i < row + 8; i += 1) {
-        const currentRow = plates[i];
+      // 검은색 칸부터 시작하는 경우
+      for (let i = 0; i < 8; i += 1) {
+        const currentRow = plates[i + row];
+        const comparedRow = i % 2 === 0 ? blackFirstRow : whiteFirstRow;
 
-        const bCount = currentRow.filter(
-          (e, idx) => e === 'B' && idx >= col && idx < col + 8,
-        ).length;
-
-        count += Math.abs(4 - bCount);
+        for (let j = 0; j < 8; j += 1) {
+          if (comparedRow[j] !== currentRow[j + col]) {
+            blackFirstCount += 1;
+          }
+        }
       }
 
-      if (!minCount || count < minCount) {
-        minCount = count;
+      // 흰색 칸부터 시작하는 경우
+      for (let i = 0; i < 8; i += 1) {
+        // 실제 입력된 배열에서의 row 인덱스 = i + row
+        const currentRow = plates[i + row];
+        const comparedRow = i % 2 === 0 ? whiteFirstRow : blackFirstRow;
+
+        for (let j = 0; j < 8; j += 1) {
+          // 실제 입력된 배열에서의 col 인덱스 = j + col
+          if (comparedRow[j] !== currentRow[j + col]) {
+            whiteFirstCount += 1;
+          }
+        }
       }
+
+      minCount = Math.min(minCount, blackFirstCount, whiteFirstCount);
     }
   }
 
