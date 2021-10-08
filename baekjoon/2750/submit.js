86c574a585e12c1784019d17357426ae9cce0247
kyh196201 <kyh196201@gmail.com>
2021-10-08 17:21:19 +0900

    백준 2750번 버블 정렬로 풀이

--- baekjoon/2750/submit.js
+++ baekjoon/2750/submit.js
@@ -7,8 +7,8 @@ const rl = readline.createInterface({
 
 const input = [];
 
-// 오름차순 버블 정렬
-const bubbleSort = (arr = [], sortFn = (a, b) => a - b > 0) => {
+// 오름차순
+const sorting = (arr = [], sortFn = (a, b) => a - b > 0) => {
   if (!arr.length) return arr;
 
   const sorted = arr.slice();
@@ -27,10 +27,32 @@ const bubbleSort = (arr = [], sortFn = (a, b) => a - b > 0) => {
   return sorted;
 };
 
+// 버블 정렬 오름 차순
+const bubbleSort = arr => {
+  const sorted = arr.slice();
+  const arrLength = sorted.length;
+  let temp = null;
+
+  for (let i = 0; i < arrLength - 1; i += 1) {
+    for (let j = 0; j < arrLength - (i + 1); j += 1) {
+      if (sorted[j] > sorted[j + 1]) {
+        temp = sorted[j + 1];
+        sorted[j + 1] = sorted[j];
+        sorted[j] = temp;
+      }
+    }
+  }
+
+  return sorted;
+};
+
 rl.on('line', line => {
   input.push(line);
 }).on('close', () => {
-  console.log(bubbleSort(input.slice(1).map(v => +v)).join('\n'));
+  const origin = input.slice(1).map(v => +v);
+  const sorted = bubbleSort(origin);
+
+  console.log(sorted.join('\n'));
 
   process.exit();
 });
