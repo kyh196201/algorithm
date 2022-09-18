8c0b6ac12468f8c260266a2ee7c4577aa662062d
seungwoo Kim <kyh196201@gmail.com>
2022-09-18 23:44:02 +0900

    프로그래머스 82612번 예외 케이스 추가

--- Programmers/Level1/82612/app.test.js
+++ Programmers/Level1/82612/app.test.js
@@ -16,6 +16,8 @@
  * - 현재 가지고있는 금액에서 총 금액을 뺀 값이 구하고자하는 값이다.
  *
  * 반성
+ * - 금액이 부족하지 않을 경우 0을 리턴해야하는데, 총 금액과 money가 같을 경우만 생각했다.
+ * money가 더 클 경우에도 0을 반환해야했다.
  */
 function getTotalPrice(price = 1, count = 1) {
   return price * ((count * (count + 1)) / 2);
@@ -32,11 +34,15 @@ describe('getTotalPrice', () => {
 function solution(price = 1, money = 1, count = 1) {
   const totalPrice = getTotalPrice(price, count);
 
-  return totalPrice - money;
+  return Math.max(totalPrice - money, 0);
 }
 
 describe('부족한 금액 계산하기', () => {
   it('test', () => {
     expect(solution(3, 20, 4)).toBe(10);
+
+    expect(solution(3, 30, 4)).toBe(0);
+
+    expect(solution(3, 35, 4)).toBe(0);
   });
 });
