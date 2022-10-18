97e482770782caa79fcc27771d5c33325f40bb54
seungwoo Kim <kyh196201@gmail.com>
2022-10-19 00:32:45 +0900

    프로그래머스 12953번 다른 풀이

--- Programmers/Level2/12953/app.test.js
+++ Programmers/Level2/12953/app.test.js
@@ -20,6 +20,8 @@
  * - 최소공배수를 구하고 계속 저장할 변수를 선언한다는 것 까지는 생각했는데, 초기 값을 어떻게 설정해야할지 고민되었다.
  * - null 또는 0으로 설정하고, 값이 없을 경우에 이전 인덱스의 값과 비교할까라고 생각했는데, 첫번째 숫자를 초기값으로 설정하면 되겠다라고 떠올라서 문제를 풀 수 있었다.
  * - 다른 사람들의 풀이를 참고했는데, reduce를 이용해서도 문제를 풀 수 있었다.
+ *
+ * - 최대공약수, 최소공배수 알고리즘은 절대 까먹지 말자
  */
 
 function getGcd(num1, num2) {
@@ -50,12 +52,22 @@ function solution(numbers = []) {
   return lcm;
 }
 
+function solutionReduce(numbers = []) {
+  if (numbers.length < 2) return numbers[0];
+
+  return numbers.reduce((lcm, cur) => getLcm(lcm, cur), numbers[0]);
+}
+
 describe('N개의 최소공배수', () => {
   it('test', () => {
+    // for문을 이용한 풀이
     expect(solution([100])).toBe(100);
-
     expect(solution([2, 6, 8, 14])).toBe(168);
-
     expect(solution([1, 2, 3])).toBe(6);
+
+    // reduce를 이용한 풀이
+    expect(solutionReduce([100])).toBe(100);
+    expect(solutionReduce([2, 6, 8, 14])).toBe(168);
+    expect(solutionReduce([1, 2, 3])).toBe(6);
   });
 });
