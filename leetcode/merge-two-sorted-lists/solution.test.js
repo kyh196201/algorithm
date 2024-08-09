a9ebf991457fffd013a171aac8493463b2ad2278
kyh196201 <kyh196201@gmail.com>
2024-08-09 15:12:19 +0900

    leetcode merge-two-sorted-lists 풀이1 수정

--- leetcode/merge-two-sorted-lists/solution.test.js
+++ leetcode/merge-two-sorted-lists/solution.test.js
@@ -25,57 +25,47 @@ class ListNode {
  * @return {ListNode}
  */
 function mergeTwoLists(list1, list2) {
-  if (!list1 && !list2) {
-    return null;
+  if (list1 === null) {
+    return list2;
+  }
+
+  if (list2 === null) {
+    return list1;
   }
 
   let head = null;
-  let tail = head;
+  let tail = null;
 
+  // head, tail의 초기값 설정
+  if (list1.val <= list2.val) {
+    head = list1;
+    tail = head;
+    list1 = list1.next;
+  } else {
+    head = list2;
+    tail = head;
+    list2 = list2.next;
+  }
+
+  // list1, list2 둘 다 있을 경우 각 노드 중에 더 작은 노드를
+  // 새로운 리스트에 노드를 연결한다
   while (list1 && list2) {
-    const node = new ListNode();
-
     if (list1.val <= list2.val) {
-      node.val = list1.val;
+      tail.next = list1;
+      // tail은 마지막 노드를 가리킨다
+      tail = tail.next;
       list1 = list1.next;
     } else {
-      node.val = list2.val;
+      tail.next = list2;
+      tail = tail.next;
       list2 = list2.next;
     }
-
-    if (head === null) {
-      head = node;
-      tail = node;
-    } else {
-      tail.next = node;
-      tail = tail.next;
-    }
   }
 
-  while (list1) {
-    const node = new ListNode(list1.val);
-    if (head === null) {
-      head = node;
-      tail = node;
-    } else {
-      tail.next = node;
-      tail = tail.next;
-    }
-
-    list1 = list1.next;
-  }
-
-  while (list2) {
-    const node = new ListNode(list2.val);
-    if (head === null) {
-      head = node;
-      tail = node;
-    } else {
-      tail.next = node;
-      tail = tail.next;
-    }
-
-    list2 = list2.next;
+  if (list1) {
+    tail.next = list1;
+  } else if (list2) {
+    tail.next = list2;
   }
 
   return head;
@@ -100,11 +90,27 @@ function createList(nums = []) {
 }
 
 describe('merge-two-sorted-lists', () => {
-  it('test', () => {
-    const list1 = createList([1, 2, 4]);
-    const list2 = createList([1, 3, 4]);
-    const expected = createList([1, 1, 2, 3, 4, 4]);
+  describe('solution 1', () => {
+    it('test1', () => {
+      const list1 = createList([1, 2, 4]);
+      const list2 = createList([1, 3, 4]);
+      const expected = createList([1, 1, 2, 3, 4, 4]);
 
-    expect(mergeTwoLists(list1, list2)).toEqual(expected);
+      expect(mergeTwoLists(list1, list2)).toEqual(expected);
+    });
+
+    it('test2', () => {
+      expect(mergeTwoLists(null, null)).toEqual(null);
+    });
+
+    it('test3', () => {
+      expect(mergeTwoLists(null, createList([0]))).toEqual(createList([0]));
+    });
+
+    it('test4', () => {
+      expect(mergeTwoLists(createList([0, 1]), null)).toEqual(
+        createList([0, 1]),
+      );
+    });
   });
 });
