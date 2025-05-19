b15784613fbebf297f717e54a6653b427a8f2c2d
Seungwoo Kim <kyh196201@gmail.com>
2025-05-20 00:30:39 +0900

    프로그래머스 42584번 다른 풀이

--- Programmers/Level2/42584/solution.test.js
+++ Programmers/Level2/42584/solution.test.js
@@ -23,10 +23,38 @@ function solution(prices = []) {
   return answer;
 }
 
+function solution2(prices) {
+  const answer = [];
+
+  for (let i = 0; i < prices.length; i++) {
+    let found = false;
+
+    for (let j = i + 1; j < prices.length; j++) {
+      if (prices[i] > prices[j]) {
+        answer[i] = j - i;
+        found = true;
+        break;
+      }
+    }
+
+    if (!found) {
+      answer[i] = prices.length - 1 - i;
+    }
+  }
+
+  return answer;
+}
+
 describe('주식가격', () => {
   it('test', () => {
     expect(solution([1, 2, 3, 2, 3])).toEqual([4, 3, 1, 1, 0]);
     expect(solution([3, 2, 1])).toEqual([1, 1, 0]);
     expect(solution([1, 4, 5, 6, 2, 1])).toEqual([5, 3, 2, 1, 1, 0]);
+  });
+
+  it('test2', () => {
+    expect(solution2([1, 2, 3, 2, 3])).toEqual([4, 3, 1, 1, 0]);
+    expect(solution2([3, 2, 1])).toEqual([1, 1, 0]);
+    expect(solution2([1, 4, 5, 6, 2, 1])).toEqual([5, 3, 2, 1, 1, 0]);
   });
 });
