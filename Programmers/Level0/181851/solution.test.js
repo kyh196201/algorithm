6d49b7f4c2b82e0d89f8abdfd1e18af7974a9ffd
Seungwoo Kim <kyh196201@gmail.com>
2025-08-22 01:20:47 +0900

    프로그래머스 181851번 다른 풀이

--- Programmers/Level0/181851/solution.test.js
+++ Programmers/Level0/181851/solution.test.js
@@ -15,10 +15,43 @@ function solution(rank, attendance) {
   return 10000 * a + 100 * b + c;
 }
 
+function solution2(rank, attendance) {
+  const attends = [];
+
+  let currentRank = 1;
+  const lastRank = rank.length;
+
+  // rank.length가 N이라면, rank에는 1 ~ N까지 정수가 담겨 있음
+  // 참석하고, 가장 등수가 높은 3명을 찾을 때까지 반복
+  while (attends.length < 3 && currentRank <= lastRank) {
+    // 현재 순서
+    const index = rank.indexOf(currentRank);
+
+    if (index > -1 && attendance[index]) {
+      attends.push(index);
+    }
+
+    currentRank++;
+  }
+
+  const [a, b, c] = attends;
+
+  return 10000 * a + 100 * b + c;
+}
+
 describe('전국 대회 선발 고사', () => {
   it('test', () => {
     expect(
       solution(
+        [3, 7, 2, 5, 4, 6, 1],
+        [false, true, true, true, true, false, false],
+      ),
+    ).toBe(20403);
+  });
+
+  it('test2', () => {
+    expect(
+      solution2(
         [3, 7, 2, 5, 4, 6, 1],
         [false, true, true, true, true, false, false],
       ),
